c9a10ac607ea5ef517dc3dee413508f176bb8c9d
Morten Rosenquist <201706031@post.au.dk>
2019-03-13 11:12:33 +0100

    Tilføjet server funktioner

---
+++ UDP_Server/Server.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDP_Server
+{
+    public class Server
+    {
+        private const int bufSize = 8 * 1024;
+        private Socket _socket;
+        private State _state;
+        private EndPoint epFrom;
+        private AsyncCallback recv = null;
+
+        public class State
+        {
+            public byte[] buffer = new byte[bufSize];
+        }
+
+        public Server(string address, int port)
+        {
+            epFrom = new IPEndPoint(IPAddress.Any, 0);
+            _state = new State();
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram,ProtocolType.Udp);
+            _socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.ReuseAddress, true);
+            _socket.Bind(new IPEndPoint(IPAddress.Parse(address), port));
+            
+            Receive();
+           
+        }
+
+        public void Send(string text)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(text);
+            _socket.BeginSend(data,0,data.Length,SocketFlags.None, (ar) =>
+            {
+                State so = (State)ar.AsyncState;
+                int bytes = _socket.EndSend(ar);
+                Console.WriteLine("SEND: {0}, {1}", bytes, text);
+            }, _state);
+        }
+
+        private void Receive()
+        {
+            _socket.BeginReceiveFrom(_state.buffer, 0, bufSize, SocketFlags.None, ref epFrom, recv = (ar) =>
+            {
+                State so = (State) ar.AsyncState;
+                int bytes = _socket.EndReceiveFrom(ar, ref epFrom);
+                _socket.BeginReceiveFrom(so.buffer, 0, bufSize, SocketFlags.None, ref epFrom, recv, so);
+                Console.WriteLine("RECV: {0}: {1}, {2}", epFrom.ToString(), bytes,
+                    Encoding.ASCII.GetString(so.buffer, 0, bytes));
+            }, _state);
+
+        }
+
+    }
+}
